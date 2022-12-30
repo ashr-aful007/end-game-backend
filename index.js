be43988c651ef9d5fabfeab0e73870f4c8f115de
ashr-aful007 <ashraful99980@gmail.com>
2022-12-30 20:16:06 +0600

    change

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json())
 //endGameDB
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.bsfuvd2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
